691c4f1ded9ae158851277af100abd794561c140
James Campbell <jaymznis@gmail.com>
2021-06-01 13:30:08 -0400

    Add project files.

---
+++ OneDayChallenge.Web.API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace OneDayChallenge.Web.API.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
