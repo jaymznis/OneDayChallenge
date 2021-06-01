ed5d7548f4e05b20ae153cbe93c91e7651c8b6ee
Taylor <taylorfulner@gmail.com>
2021-06-01 15:31:08 -0400

    comment built

--- OneDayChallenge.Data/Comment.cs
+++ OneDayChallenge.Data/Comment.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,19 @@ namespace OneDayChallenge.Data
 {
     public class Comment
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public Guid AuthorId { get; set; }
+
+        public virtual List<Reply> Replies { get; set; }
+
+        [ForeignKey(nameof(Post.Id))]
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
     }
 }
